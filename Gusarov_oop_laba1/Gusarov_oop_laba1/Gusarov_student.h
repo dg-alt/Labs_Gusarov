0d054814d086b9f314547e1d7ab8a1b5d720f993
dg-alt <dagusar@mail.ru>
2022-10-05 13:45:11 +0300

    #2
    second header

--- Gusarov_oop_laba1/Gusarov_oop_laba1/Gusarov_student.h
+++ Gusarov_oop_laba1/Gusarov_oop_laba1/Gusarov_student.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <fstream>
+class Gusarov_student
+{
+public:
+	char name[50];
+	int age;
+	void setAttributes();
+	void showAttributes();
+	void Load(std::fstream&);
+	void Save(std::fstream&);
+};
+
+
